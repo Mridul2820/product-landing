624664e5ed679a570c373e41819f274e5b5c709a
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-30 10:07:48 +0530

    footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { StyledButton } from './GlobalStyles'
 
@@ -8,16 +9,49 @@ const Footer = () => {
             <section>
                 <h3>Join our exclusive membership to receive the latest news and trends</h3>
                 <p>You can unsubscribe at any time.</p>
-                <StyledForm>
+                <FooterForm>
                     <input 
                         name='email' 
                         type='email' 
                         placeholder='Your Email' 
                     />
                     <StyledButton fontBig>Subscribe</StyledButton>
-                </StyledForm>
+                </FooterForm>
             </section>
-            
+            <FooterLinkWrap>
+                <FooterLinks>
+                    <h2>About Us</h2>
+                    <FooterLink to='/sign-up'>How it works</FooterLink>
+                    <FooterLink to='/'>Testimonials</FooterLink>
+                    <FooterLink to='/'>Careers</FooterLink>
+                    <FooterLink to='/'>Investors</FooterLink>
+                    <FooterLink to='/'>Terms of Service</FooterLink>
+                </FooterLinks>
+
+                <FooterLinks>
+                    <h2>Contact Us</h2>
+                    <FooterLink to='/'>Contact</FooterLink>
+                    <FooterLink to='/'>Support</FooterLink>
+                    <FooterLink to='/'>Destinations</FooterLink>
+                    <FooterLink to='/'>Sponsorships</FooterLink>
+                </FooterLinks>
+
+                <FooterLinks>
+                    <h2>Videos</h2>
+                    <FooterLink to='/'>Submit Video</FooterLink>
+                    <FooterLink to='/'>Ambassadors</FooterLink>
+                    <FooterLink to='/'>Agency</FooterLink>
+                    <FooterLink to='/'>Influencer</FooterLink>
+                </FooterLinks>
+
+                <FooterLinks>
+                    <h2>Social Media</h2>
+                    <FooterLink to='/'>Instagram</FooterLink>
+                    <FooterLink to='/'>Facebook</FooterLink>
+                    <FooterLink to='/'>Youtube</FooterLink>
+                    <FooterLink to='/'>Twitter</FooterLink>
+                </FooterLinks>
+            </FooterLinkWrap>
         </StyledFooter>
     )
 }
@@ -61,7 +95,7 @@ export const StyledFooter = styled.div`
     }
 `
 
-const StyledForm = styled.form`
+const FooterForm = styled.form`
     display: flex;
     justify-content: center;
     align-items: center;
@@ -92,6 +126,52 @@ const StyledForm = styled.form`
             padding: 10px;
             font-size: 17px;
         }
+    }
+`
+
+const FooterLinkWrap = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 20px;
+
+    @media screen and (max-width: 768px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+`
+
+const FooterLinks = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    text-align: left;
+    width: 160px;
+    color: #fff;
+
+    @media screen and (max-width: 425px) {
+        width: 120px;
+    }
+
+    h2 {
+        margin-bottom: 16px;
+
+        @media screen and (max-width: 425px) {
+            font-size: 18px;
+        }
+    }
+`
+
+const FooterLink = styled(Link)`
+    color: #fff;
+    text-decoration: none;
+    margin-bottom: 0.5rem;
+
+    @media screen and (max-width: 425px) {
+        font-size: 14px;
+    }
+
+    &:hover {
+        color: #0467fb;
+        transition: 0.3s ease-out;
     }
 `
 
